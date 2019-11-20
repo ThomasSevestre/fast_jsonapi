43843e0cf6a044ee43fad0720d4d8001dfc9f48a
Thomas <ts@tcare.fr>
2019-11-20 13:48:53 +0100

    fix merge

--- lib/fast_jsonapi/attribute.rb
+++ lib/fast_jsonapi/attribute.rb
@@ -1,33 +1,5 @@
-<<<<<<< HEAD
-module FastJsonapi
-  class Attribute
-    attr_reader :comment
-
-    def initialize(key:, method:, options: {})
-      @key = key
-      @method = method
-      @conditional_proc = if options[:if].present?
-        options[:if]
-      else
-        nil
-      end
-      @comment= options[:comment]
-    end
-
-    def serialize(record, serialization_params, output_hash)
-      if @conditional_proc.nil? || @conditional_proc.call(record, serialization_params)
-        output_hash[@key] = if @method.is_a?(Proc)
-          @method.arity.abs == 1 ? @method.call(record) : @method.call(record, serialization_params)
-        else
-          record.public_send(@method)
-        end
-      end
-    end
-  end
-=======
 require 'fast_jsonapi/scalar'
 
 module FastJsonapi
   class Attribute < Scalar; end
->>>>>>> official/master
 end
